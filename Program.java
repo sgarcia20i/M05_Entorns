dc06cda34093b467a2df07aa1dd78a93554ded4f
Stephen Garcia <sgarcia20@ilg.cat>
2023-11-30 20:29:31 +0100

    Revert "Deshacer un commit"
    This reverts commit dfbcdd05516cf43c77402e0874a1ff1cd95d95bd.

--- Program.java
+++ Program.java
@@ -4,7 +4,7 @@
  * Program to calculate the area and the perimeter
  * of a circle knowing the radius in meter.
  *
- * Copyright 2020 Joan Stephen Garcia <sgarcia20@ilg.cat>
+ * Copyright 2020 Joan Sèculi <jseculi@escoladeltreball.org>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
